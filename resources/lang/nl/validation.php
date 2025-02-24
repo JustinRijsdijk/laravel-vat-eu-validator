5637e11a631e154b067ce97244536709f204aa9a
Justin <41448351+JustinRijsdijk@users.noreply.github.com>
2025-02-24 20:19:57 +0100

    added dutch translations

---
+++ resources/lang/nl/validation.php
@@ -0,0 +1,6 @@
+<?php
+return [
+    'vat_number' => ':attribute moet een geldig BTW nummer zijn.',
+    'vat_number_format' => ':attribute moet geformatteerd zijn volgens de juiste regels {country_name}{vat_number}.',
+    'vat_number_exist' => 'Het BTW nummer: :attribute bestaat niet.',
+];
